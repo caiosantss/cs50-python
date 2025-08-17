8b1ce86e8f29871020aed2330ccb8830c4a291ff
Caio Santos <caiosilvasantos87@gmail.com>
2025-08-17 22:44:32 +0000

    Sun, Aug 17, 2025, 7:44 PM -03:00

---
+++ playback/playback.py
@@ -0,0 +1,15 @@
+## Input do usuario
+
+phrase = input("Write a phrase: " )
+
+##Split e colcoar em uma lista
+
+lista = list()
+lista.append(phrase.split(" "))
+
+
+print(("...", join(lista)))
+
+##Join
+
+
