4809cbd4960fba12f4502afd7dce21b02c6f886c
Caio Santos <caiosilvasantos87@gmail.com>
2025-09-19 00:50:26 +0000

    Thu, Sep 18, 2025, 9:50 PM -03:00

--- plates/plates.py
+++ plates/plates.py
@@ -6,47 +6,72 @@ def main():
         print("Invalid")
 
 
-def is_valid(s):
+def is_valid(plate):
+
+    if start_two_letters(plate):
+        print("start_two_letters") ## ok
+    if max_min_charaters(plate):
+        print("max_min_charaters(plate)") ## ok
+    if is_number_end(plate):
+        print("is_number_end(plate)") ## ok
+    if is_first_number_zero(plate):
+        print("is_number_end(plate)") ## ok
+    if is_alphanumerico(plate):
+        print("is_number_end(plate)")
 
 """
-1 - All vanity plates must start with at least two letters.
+OK - 1 - All vanity plates must start with at least two letters.
 OK - 2 - vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.
-3 - Numbers cannot be used in the middle of a plate they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
+OK - 3 - Numbers cannot be used in the middle of a plate they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
 OK - 4 - The first number used cannot be a 0.
 OK - 5 - No periods, spaces, or punctuation marks are allowed.
-
 """
 
-def max_min_charaters(palavra):
-    """ vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters """
+## 1 - All vanity plates must start with at least two letters.
+def start_two_letters(palavra):
+   return palavra[0:2].isalpha()
 
-    contador = 0
-    for letra in palavra:
-        contador += 1
-    if contador >= 2 and contador <= 6:
+
+## 2 -Return True if its between 2 and 6
+def max_min_charaters(palavra):
+    if len(palavra) >= 2 and len(palavra) <= 6:
         return True
     else:
-        Return False
+        return False
 
 
-def start(palavra):
-    palavra.
+## 3 Numbers cannot be used in the middle of a plate
+def is_number_end(plate):
+    find_number = False
+    for i in plate:
+        if i.isdigit():
+            find_number = True
+        elif find_number and i.isalpha():
+            return False
+    return True
 
-def is_alphanumerico(palavra):
-   return palavra.isalnum()
 
-    """“No periods, spaces, or punctuation marks are allowed.”"""
-
-
+## 4 First number cannot be 0
 def is_first_number_zero(palavra):
     contador = 0
     for numero in palavra:
         if numero.isdecimal() and contador == 0:
             if numero == '0':
-                contador += 1
                 return True
             else:
-                contador += 1
                 return False
+        contador += 1
+
+ ## 5 No periods, spaces, or punctuation marks are allowed:
+def is_alphanumerico(palavra):
+   return palavra.isalnum()
+##is alphanumeric - only letters and numbers
+
+
+main()
+
+
+
+
 
 
