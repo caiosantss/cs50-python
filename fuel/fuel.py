5a91b9f3b8a85851930b06c12e94adfa211ed369
Caio Santos <caiosilvasantos87@gmail.com>
2025-09-21 23:39:23 +0000

    Sun, Sep 21, 2025, 8:39 PM -03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,9 +1,9 @@
 ## input x/y
 
-tank = int(input("Fraction: ")).split("/")
-x = tank[0]
-y = tank[1]
+tank = input("Fraction: ").split("/")
+x = int(tank[0])
+y = int(tank[1])
 
-print(f"{x} / {y}")
+
 
 
