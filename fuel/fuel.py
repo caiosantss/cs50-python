91057d44191ec97d06c27de8406c613fdc7e520d
Caio Santos <caiosilvasantos87@gmail.com>
2025-09-21 23:43:42 +0000

    Sun, Sep 21, 2025, 8:43 PM -03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,9 +1,19 @@
 ## input x/y
 
-tank = input("Fraction: ").split("/")
-x = int(tank[0])
-y = int(tank[1])
 
+
+
+
+while True:
+    try:
+        tank = input("Fraction: ").split("/")
+        x = int(tank[0])
+        y = int(tank[1])
+    except ValueError:
+        break
+    except ZeroDivisionError:
+        break
+    
 
 
 
