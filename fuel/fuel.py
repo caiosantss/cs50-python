4f8d5d773b3ba9e4fe4b185ea749997f3f12e97f
Caio Santos <caiosilvasantos87@gmail.com>
2025-09-21 23:37:32 +0000

    Sun, Sep 21, 2025, 8:37 PM -03:00

---
+++ fuel/fuel.py
@@ -0,0 +1,9 @@
+## input x/y
+
+tank = int(input("Fraction: ")).split("/")
+x = tank[0]
+y = tank[1]
+
+print(f"{x} / {y}")
+
+
---
+++ nutrition/nutrition.py
@@ -0,0 +1,43 @@
+
+def main():
+    fruits = {
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80
+}
+
+    fruit = input("Item: ").lower().strip()
+
+    print(check_fruit_calories(fruit, fruits))
+
+
+
+def check_fruit_calories(fruit, fruits):
+    if fruit in fruits:
+        return(f"Calories: {fruits[fruit]}")
+    else:
+        return ""
+
+main()
+
+
+
+
+
