2eae1ada644b731302f6b12fd3c78f1e0e87cf51
Caio Santos <caiosilvasantos87@gmail.com>
2025-09-22 00:21:36 +0000

    Sun, Sep 21, 2025, 9:21 PM -03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -11,7 +11,7 @@ def tank():
             tank = input("Fraction: ").split("/")
             x = convert_int(tank[0])
             y = convert_int(tank[1])
-            if x < y and y != 0 and x > 0:
+            if x =< y and y != 0 and x > 0:
                 return f"{show_percentage(x, y)}%"
         except ValueError:
             pass
@@ -24,13 +24,13 @@ def convert_int(n):
 
 
 def show_percentage(n1, n2):
-    return int((n1 / n2) * 100)
-
-def e_or_f(percentage):
-    if percentage <= 1:
+    porcentagem = int((n1 / n2) * 100)
+    if porcentagem <= 1:
         return "E"
-    elif percentage >= 99:
+    elif porcentagem >= 99:
         return "F"
+    else:
+        return porcentagem
 
 
 
