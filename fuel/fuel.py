412d8584beb8c6dce546fd7f9ead852a42989f34
Caio Santos <caiosilvasantos87@gmail.com>
2025-09-22 00:56:27 +0000

    Sun, Sep 21, 2025, 9:56 PM -03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -6,22 +6,15 @@ def tank():
     while True:
         try:
             fraction = input("Fraction: ")
-
-
+            parts = fraction.split("/")
+            if len(parts) != 2:
+                continue
+            x = int(parts[0])
+            y = int(parts[1])
+            if x <= y and x >= 0:
                 return show_percentage(x, y)
         except (ValueError, ZeroDivisionError):
             pass
-
-
-def valid_fraction(fraction):
-    parts = fraction.split("/")
-        if len(parts) != 2:
-            x = int(parts[0])
-            y = int(parts[1])
-                if x <= y and x >= 0:
-                    
-
-
 
 def show_percentage(n1, n2):
         porcentagem = round((n1 / n2) * 100)
