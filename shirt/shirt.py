61ae9eaa69d7907c1e90052f6f7dc241fd2dc698
Caio Santos <caiosilvasantos87@gmail.com>
2025-10-31 16:28:24 +0000

    Fri, Oct 31, 2025, 1:28 PM -03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -2,5 +2,6 @@ from PIL import Image
 
 with Image.open("before1.jpg") as image:
     print(image.format, image.size, image.mode)
-    new_image_before1 = image.thumbnail((600, 600))
-    new_image_before1.save("New_thumbnail_before1_resize.jpg")
+    #Crop porporcional - com save na propria imagem usando nome diferente
+    # image.thumbnail((600, 600))
+    # image.save("thumbnail before1 600.jpg")
