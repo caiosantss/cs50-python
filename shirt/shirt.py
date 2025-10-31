394dd570ed18a4efc406a289a1030a5b46f138a3
Caio Santos <caiosilvasantos87@gmail.com>
2025-10-31 16:21:07 +0000

    Fri, Oct 31, 2025, 1:21 PM -03:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -2,4 +2,5 @@ from PIL import Image
 
 with Image.open("before1.jpg") as image:
     print(image.format, image.size, image.mode)
-    new_image_before1 = image.thumbnail((600, 600)).save("New_thumbnail_before1_resize.jpg")
+    new_image_before1 = image.thumbnail((600, 600))
+    new_image_before1.save("New_thumbnail_before1_resize.jpg")
