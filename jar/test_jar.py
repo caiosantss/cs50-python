cbaeb145e115591b0b9352f37d06d081550455ef
Caio Santos <caiosilvasantos87@gmail.com>
2025-11-27 16:18:37 +0000

    Thu, Nov 27, 2025, 1:18 PM -03:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -3,7 +3,14 @@ import pytest
 
 
 def test_init():
-    ...
+    jar = Jar()
+    asssert jat.capacity == 12
+    assert jar.size == 0
+
+    # negative capacity
+
+    with pytest.raises(ValueError):
+        Jar(-1)
 
 
 def test_str():
@@ -33,4 +40,20 @@ def test_deposit():
 
 def test_withdraw():
     jar = Jar()
-    jar.deposit()
+    jar.deposit(5)
+    jar.withdraw(3)
+    assert jar.size == 2
+    jar.withdraw(2)
+    assert jar.size == 0
+
+    ## -- Size: 5
+    jar.deposit(5)
+
+    # n < 0
+    with pytest.raises(ValueError):
+        jar.withdraw(-1)
+
+    # n > size
+    with pytest.raises(ValueError):
+        jar.withdraw(6)
+
