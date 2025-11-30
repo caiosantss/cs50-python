723fb202766fd218c0813d7cd3c7ab426f72721b
Caio Santos <caiosilvasantos87@gmail.com>
2025-11-30 20:28:02 +0000

    Sun, Nov 30, 2025, 5:28 PM -03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -2,4 +2,19 @@ import datetime
 
 date_iso_format = "2025-12-25"
 
+#Converte para date object -- Somente a data
 print(datetime.date.fromisoformat(date_iso_format))
+
+#converte para datetime object - Data + Hora(00:00:00)
+data_convertida = datetime.datetime.strptime(date_iso_format, "%Y-%m-%d")
+print(data_convertida)
+
+agora = datetime.datetime.now()
+print(agora)
+
+#Diferença entre datas
+diferenca = data_convertida - agora
+print(diferenca)
+
+minutos = diferenca.total_seconds() / 60
+print(f"{minutos:.2f}")
